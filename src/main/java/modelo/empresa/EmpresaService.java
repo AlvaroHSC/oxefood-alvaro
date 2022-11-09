0f9fe93426a05a6c7ba90136e127ef591411aad9
aluno <ifpe@aluno>
2022-11-08 21:34:08 -0300

    atualizacso 08112022

--- src/main/java/modelo/empresa/EmpresaService.java
+++ src/main/java/modelo/empresa/EmpresaService.java
@@ -1,5 +1,7 @@
 package modelo.empresa;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,7 +9,15 @@ import br.com.ifpe.oxefoodalvaro.utils.entity.GenericService;
 
 @Service
 public class EmpresaService extends GenericService {
-	
+
 	@Autowired
+	private EmpresaRepository repository;
+
+	@Transactional
+    public Empresa save(Empresa empresa) {
+
+	super.preencherCamposAuditoria(empresa);
+	return repository.save(empresa);
+	}
 
 }
